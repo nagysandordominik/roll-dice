ceee0a75986b8c8b392abf8431f33f2b2016725d
Nagy Sándor Dominik <thesatyka@gmail.com>
2022-10-06 23:34:38 +0200

    add: state for click and displaying rolling text

--- roll-dice/src/RollDice.js
+++ roll-dice/src/RollDice.js
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
-import Die from "./Die";
+import Die from './Die';
+import './RollDice.css'
 
 class RollDice extends Component {
     static defaultProps = {
@@ -7,7 +8,7 @@ class RollDice extends Component {
     };
     constructor(props) {
         super(props);
-        this.state = { die1 : "one", die2: "one"};
+        this.state = { die1 : "one", die2: "one", rolling: false};
         this.roll = this.roll.bind(this);
     }
     roll()
@@ -19,14 +20,18 @@ class RollDice extends Component {
             Math.floor(Math.random() * this.props.sides.length)
         ];
 
-        this.setState({die1: newDie1, die2: newDie2})
+        this.setState({die1: newDie1, die2: newDie2, rolling: true})
     }
     render() {
         return(
-            <div>
-                <Die face={this.state.die1}/>
-                <Die face={this.state.die2}/>
-                <button onClick={this.roll}>Roll dice</button>
+            <div className="RollDice">
+                <div className="RollDice-container">
+                    <Die face={this.state.die1}/>
+                    <Die face={this.state.die2}/>
+                </div>
+                <button onClick={this.roll}>
+                    {this.state.rolling ? 'Rolling...' : 'Roll dice!'}
+                </button>
             </div>
         )
     }
